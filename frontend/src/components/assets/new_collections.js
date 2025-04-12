91536f3e8f3b98a5bcdcb83c4bc317835a471deb
dharanidharan-palanisamy <bharanidharan2005@gmail.com>
2025-04-12 06:37:45 +0530

    Initial commit

--- frontend/src/components/assets/new_collections.js
+++ frontend/src/components/assets/new_collections.js
@@ -1,69 +1,95 @@
-import p1_img from "./product_13.png";
-import p2_img from "./product_35.png";
-import p3_img from "./product_14.png";
-import p4_img from "./product_16.png";
-import p5_img from "./product_15.png";
-import p6_img from "./product_19.png";
-import p7_img from "./product_17.png";
-import p8_img from "./product_28.png";
+import p1_img from "./product_1.jpg";
+import p2_img from "./product_2.png";
+import p3_img from "./product_3.jpg";
+import p4_img from "./product_4.jpg";
+import p5_img from "./product_5.jpg";
+import p6_img from "./product_6.jpg";
+import p26_img from "./product_26.png";
+import p27_img from "./product_27.png";
+import p30_img from "./product_30.png";
+import p31_img from "./product_31.png";
 
 let new_collections = [
-  {
-    id: 12,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-    image: p1_img,
-    new_price: 50.0,
-    old_price: 80.5,
-  },
-  {
-    id: 35,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
-    image: p2_img,
-    new_price: 85.0,
-    old_price: 120.5,
-  },
-  {
-    id: 14,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
-    image: p3_img,
-    new_price: 60.0,
-    old_price: 100.5,
-  },
-  {
-    id: 8,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-    image: p4_img,
-    new_price: 100.0,
-    old_price: 150.0,
-  },
-  {
-    id: 15,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
-    image: p5_img,
-    new_price: 50.0,
-    old_price: 80.5,
-  },
-  {
-    id: 2,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-    image: p6_img,
-    new_price: 85.0,
-    old_price: 120.5,
-  },
-  {
-    id: 17,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
-    image: p7_img,
-    new_price: 60.0,
-    old_price: 100.5,
-  },
-  {
-    id: 28,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
-    image: p8_img,
-    new_price: 100.0,
-    old_price: 150.0,
-  },
+   {
+      id: 1,
+      name: "Formal Shirt",
+      category: "men",
+      image: p1_img,
+      new_price: 4000.0,
+      old_price: 500.0,
+    },
+    {
+      id: 2,
+      name: "T shirt & Jacket",
+      category: "men",
+      image: p2_img,
+      new_price: 1000.0,
+      old_price: 1200.5,
+    },
+    {
+      id: 3,
+      name: "coat & shirt",
+      category: "men",
+      image: p3_img,
+      new_price: 2500.0,
+      old_price: 3000.0,
+    },
+    {
+      id: 4,
+      name: "T Shirt",
+      category: "men",
+      image: p4_img,
+      new_price: 350.0,
+      old_price: 450.0,
+    },
+    {
+      id: 5,
+      name: "Coat Shot",
+      category: "men",
+      image: p5_img,
+      new_price: 5500.0,
+      old_price: 6000.,
+    },
+    {
+      id: 6,
+      name: "Formal Paint",
+      category: "men",
+      image: p6_img,
+      new_price: 350.0,
+      old_price: 400.0,
+    },
+    {
+        id: 26,
+        name: "T Shirt",
+        category: "kids",
+        image: p26_img,
+        new_price: 450.0,
+        old_price: 500.0,
+      },
+      {
+        id: 27,
+        name: "Hoodies",
+        category: "kids",
+        image: p27_img,
+        new_price: 850.0,
+        old_price: 1000.0,
+      },
+      {
+        id: 30,
+        name: "Jacket",
+        category: "kids",
+        image: p30_img,
+        new_price: 2500.0,
+        old_price: 3000.0,
+      },
+      {
+        id: 31,
+        name: "Jeans Shirts",
+        category: "kids",
+        image: p31_img,
+        new_price: 750.0,
+        old_price: 850.0,
+      },
 ];
 
 export default new_collections;
