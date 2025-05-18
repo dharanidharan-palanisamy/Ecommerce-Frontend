d0193a5e55327fb8f76ae0c9f73156190dcfb105
dharanidharan-palanisamy <bharanidharan2005@gmail.com>
2025-05-18 11:47:01 +0530

    Initial commit

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async()=>{
-    await mongoose.connect(process.env.MONGODB_URI).then(()=>{
-       //console.log('DB connected');
-    }).catch(err=>console.log(err));
-}
-
-module.exports=connectDB;
--- backend/routes/subscriberRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const subscriberController = require("./../controllers/subscriberController");
-
-const router = express.Router();
-
-router.post("/", subscriberController.subscribe);
-
-module.exports = router;
